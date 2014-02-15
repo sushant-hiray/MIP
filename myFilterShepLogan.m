a0991ec0ced729f053d129256f489901cd61f868
Nilesh <nileshsatishkulkarni@gmail.com>
2014-02-16 02:01:40 +0530

    fds

--- myFilterShepLogan.m
+++ myFilterShepLogan.m
@@ -7,7 +7,7 @@ FFTImage = fft2(RadonImage);
 absW = (arrayfun(@(x) abs(x),FFTImage));
 wmax= max(max(absW))
 %disp(wmax)
-wmax =wmax/2
+%wmax =wmax/2
 filter = arrayfun(@(x) sinc(x*05*pi/wmax)*x*abs(x),FFTImage); % shep logan
 
 IFFTImage = ifft2(filter);
