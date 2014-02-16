09140b45ccd2fae3b0fab7cb0b9ab333a2f6a8ed
sushant-hiray <hiraysushant@gmail.com>
2014-02-16 15:16:54 +0530

    Part 2 completed

--- 2/code/myFilterCosineFilter.m
+++ 2/code/myFilterCosineFilter.m
@@ -1,19 +1,38 @@
-function y = myFilterCosineFilter(Image,L)
-%L = 50000000;
-RadonImage = radon(Image);
-FFTImage = fft2(RadonImage);
-
-absW = (arrayfun(@(x) abs(x),FFTImage));
-wmax= max(max(absW))
-
-filter = arrayfun(@(x) cos(x*05*pi/wmax)*x*abs(x),FFTImage);
-
-IFFTImage = ifft2(filter);
-
-IRadonImage = iradon(IFFTImage,0:179);
-
-imagesc(IRadonImage);
+function y = myFilterCosineFilter(Image)
 
 
-end
+N=size(Image,1);
+theta = 0:179;
+N_theta = length(theta);
+[R,xp] = radon(Image,theta);
+
+N1 = length(xp);
+freqs=linspace(-1, 1, N1).';
+%cosine filter
+my_filter = arrayfun(@(x) cos(x*05*pi/1000)*abs(x),freqs);
+my_filter = repmat(my_filter, [1 N_theta]);
+% apply fft in one dimension and use fftshift to bring 0 frequency to
+% center
+ft_R = fftshift(fft(R,[],1),1);
+
+%apply the filter (point by point multiplication)
+filteredProj = ft_R .* my_filter;
+
+%apply the inverse fft shift (as we applied fft shift before)
+filteredProj = ifftshift(filteredProj,1);
+
+%get the real values
+ift_R = real(ifft(filteredProj,[],1));
+
+% apply inverse FBP without a filter
+I1 = iradon(ift_R, theta, 'linear', 'none', 1.0, N);
+
+subplot(1,2,2);imagesc( real(I1) ); title('Cosine filtering')
+colormap(gray(256)); axis image;
+
+
+subplot(1,2,1);imagesc( real(Image) ); title('Original Image')
+colormap(gray(256)); axis image;
+
+y = I1;
 
