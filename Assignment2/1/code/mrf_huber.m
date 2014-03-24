ab3d6438bef59eee2ce328d8dd9753ed444c45a6
sushant-hiray <hiraysushant@gmail.com>
2014-03-24 15:27:45 +0530

    Updated derivates of huber and fixed bug in quad

--- Assignment2/1/code/mrf_huber.m
+++ Assignment2/1/code/mrf_huber.m
@@ -8,10 +8,10 @@ d = circshift(A,[1,0]);
 l = circshift(A,[0,-1]);
 r = circshift(A,[0,1]);
 
-u1=abs((A-u));
-d1=abs((A-d));
-l1=abs((A-l));
-r1=abs((A-r));
+u1=abs((A-u)).*sign(A-u);
+d1=abs((A-d)).*sign(A-d);
+l1=abs((A-l)).*sign(A-l);
+r1=abs((A-r)).*sign(A-r);
 
 u1 = arrayfun(@(x) check(x,gamma),u1);
 l1 = arrayfun(@(x) check(x,gamma),l1);
@@ -22,7 +22,7 @@ y = u1+d1+l1+r1;
 
 
 function z = check(x,g)
-    if x > g
+    if abs(x) > g
       z =  g;
     else
       z = x;
--- Assignment2/1/code/mrf_quad.m
+++ Assignment2/1/code/mrf_quad.m
@@ -8,15 +8,6 @@ d = circshift(A,[1,0]);
 l = circshift(A,[0,-1]);
 r = circshift(A,[0,1]);
 
-u1=abs((A-u));
-d1=abs((A-d));
-l1=abs((A-l));
-r1=abs((A-r));
-
-u1 = arrayfun(@(x) check(x,gamma),u1);
-l1 = arrayfun(@(x) check(x,gamma),l1);
-r1 = arrayfun(@(x) check(x,gamma),r1);
-d1 = arrayfun(@(x) check(x,gamma),d1);
 
 u1=abs((A-u)).*sign(A-u);
 d1=abs((A-d)).*sign(A-d);
